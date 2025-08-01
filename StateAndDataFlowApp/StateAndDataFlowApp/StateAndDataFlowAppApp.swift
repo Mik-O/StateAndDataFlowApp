635ff734ec670c21b66c1032a209c1ecce4034c8
mikle <mikle.kontinent@gmail.com>
2025-08-01 16:35:21 +0700

    start work

---
+++ StateAndDataFlowApp/StateAndDataFlowApp/StateAndDataFlowAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowAppApp.swift
+//  StateAndDataFlowApp
+//
+//  Created by Таня Кожевникова on 01.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
