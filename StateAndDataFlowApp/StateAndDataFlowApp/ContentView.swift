07f4b8624a1c0b76dd73478093e5d31d2aec9691
mikle <mikle.kontinent@gmail.com>
2025-08-05 17:03:50 +0700

    timer working

--- StateAndDataFlowApp/StateAndDataFlowApp/ContentView.swift
+++ StateAndDataFlowApp/StateAndDataFlowApp/ContentView.swift
@@ -8,17 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var timer = TimeCounter()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(timer.counter)")
+                .font(.largeTitle)
+                .offset(x: 0, y: 100)
+                .padding()
+            Spacer()
+            
+            ButtonView()
+                .environmentObject(timer)
+            Spacer()
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct ButtonView: View {
+    @EnvironmentObject var timer: TimeCounter
+    
+    var body: some View {
+        Button(action: { timer.startTimer() }) {
+            Text("\(timer.buttonTitle)")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+        }
+        .frame(width: 200, height: 60)
+        .background(.red)
+        .cornerRadius(20)
+        .overlay(
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(Color.black, lineWidth: 4)
+        )
+        
+    }
+    
 }
